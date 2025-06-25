1e85c45763ad0db7cf3529e63199b2be4b22d8cd
Aryabhatta Dey <aryabhattadey35@gmail.com>
2025-06-25 19:51:57 +0530

    Test

--- gol.py
+++ gol.py
@@ -3,34 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def update(_, N):
-    global grid
-    global auxiliaryGrid
-    global ageGrid
-
-    for i in range(N):
-        for j in range(N):
-            total = (
-                auxiliaryGrid[(i - 1) % N, (j - 1) % N]
-                + auxiliaryGrid[(i - 1) % N, j]
-                + auxiliaryGrid[(i - 1) % N, (j + 1) % N]
-                + auxiliaryGrid[i, (j - 1) % N]
-                + auxiliaryGrid[i, (j + 1) % N]
-                + auxiliaryGrid[(i + 1) % N, (j - 1) % N]
-                + auxiliaryGrid[(i + 1) % N, j]
-                + auxiliaryGrid[(i + 1) % N, (j + 1) % N]
-            )
-
-            if auxiliaryGrid[i, j] >= 1:
-                if total in (2, 3):
-                    grid[i, j] = min(10, auxiliaryGrid[i, j] + 1)  # Increase age
-                else:
-                    grid[i, j] = 0
-            else:
-                if total == 3:
-                    grid[i, j] = 1  # Cell is reborn
-                else:
-                    grid[i, j] = 0
-
     img.set_data(grid)
     auxiliaryGrid = grid.copy()
     return [img]
